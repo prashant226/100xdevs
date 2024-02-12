b0ecb895aedaf62776077a6b253fa877c3002cab
prashant226 <prashantnaraniya2003@gmail.com>
2024-02-12 19:53:26 +0530

    commited

--- WEEK-01/main.js
+++ WEEK-01/main.js
@@ -41,8 +41,10 @@
 // } else {
 //     console.log("the num is not a even");
 // }
-const arr = [1, 2, 3, 4, 5, 6, 7, 8];
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 == 0)
-        console.log(arr[i]);
-}
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 0)
+//         console.log(arr[i]);
+// }
+
+//complex objects::
