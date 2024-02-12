1c641c5ba3a6b469ab65c72cdd1db3f5754ae036
prashant226 <prashantnaraniya2003@gmail.com>
2024-02-12 19:24:00 +0530

    commited

--- WEEK-01/main.js
+++ WEEK-01/main.js
@@ -2,5 +2,15 @@
 // console.log(a);
 
 
-var a = 1;
-console.log(a);
+// const a = 1;
+// a = 2;
+// console.log(a);
+
+let name = "hkirat";
+age = "18";
+let married = false;
+
+// console.log("the name is  " + name, "my age is" + age);
+if (married == 0) {
+    console.log(name);
+}
