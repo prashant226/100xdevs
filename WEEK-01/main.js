5d8c86ae34bf3f5007041303af95c08d0b0a5028
prashant226 <prashantnaraniya2003@gmail.com>
2024-02-12 19:43:04 +0530

    commited

--- WEEK-01/main.js
+++ WEEK-01/main.js
@@ -6,11 +6,29 @@
 // a = 2;
 // console.log(a);
 
-let name = "hkirat";
-age = "18";
-let married = false;
+// let name = "hkirat";
+// age = "18";
+// let married = false;
 
-// console.log("the name is  " + name, "my age is" + age);
-if (married == 0) {
-    console.log(name);
+// // console.log("the name is  " + name, "my age is" + age);
+// if (married == 0) {
+//     console.log(name);
+// }
+
+
+// let ans1 = 0;
+// ans1 = ans1 + 1 + 2 + 3 + 4 + 5 + 6;
+// console.log(ans1);
+
+// for (let ans = 0; ans < 6; ans++) {
+//     ans = ans + 1;
+//     console.log(ans);
+// }
+// console.log(ans);
+let ans = 0;
+
+for (let i = 0; i <= 100; i++) {
+    ans = ans + i;
+
 }
+console.log(ans);
---
+++ WEEK-01/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+for (let ans = 0; ans < 6; ans++) {
+//     ans = ans + 1;
+//     console.log(ans);
+// }
