1df85a233b977ee15d7ca94bf4f16dfedc2464d1
prashant226 <prashantnaraniya2003@gmail.com>
2024-02-13 21:37:02 +0530

    commited

--- WEEK-01/string.js
+++ WEEK-01/string.js
@@ -19,3 +19,21 @@ function getSlice(str, start, end) {
 
 }
 getSlice("hello world", 0, 5);
+
+
+function cutIT(str, startIndex, endIndex) {
+    let newStr = "";
+    for (let i = 0; i < str.length; i++) {
+        if (i >= startIndex && i < endIndex)
+            newStr = newStr + str[i];
+    }
+    return newStr;
+}
+
+const value = "harkirat singh";
+let ans2 = value.slice(2, 5)
+console.log(ans2)
+console.log(cutIT(value, 2, 5));
+const initialArray = [1, 2, 3];
+const secondArray = [4, 5, 6];
+console.log(initialArray.concat(secondArray));
