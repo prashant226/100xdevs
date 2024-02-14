c71519e07af2a7a5594fba0a3d32eae01aacee9e
prashant226 <prashantnaraniya2003@gmail.com>
2024-02-14 18:10:58 +0530

    commited

--- WEEK-01/class.js
+++ WEEK-01/class.js
@@ -19,18 +19,32 @@
 //     }
 // }
 
-class Animal {
-    constructor(name, legcount, speaks) {
-        this.name = name;
-        this.legcount = legcount;
-        this.speaks = speaks;
+// class Animal {
+//     constructor(name, legcount, speaks) {
+//         this.name = name;
+//         this.legcount = legcount;
+//         this.speaks = speaks;
 
+//     }
+//     speaks() {
+//         console.log("hi there" + this.speaks);
+//     }
+// }
+
+// console.log(Animal.myType())
+// let dog = new Animal("dog", 4, "bhow bhow");
+// let cat = new Animal("cat", 4, "meow meow");
+// cat.speaks();
+
+const currentData = new Date();
+// console.log(currentData.getDate());
+// console.log(currentData.getYear() + 1900);
+// console.log("time in milliseconds since 1970", currentData.getTime());
+
+function calculateSum() {
+    const a = 0;
+    for (let i = 0; i < 10000; i++) {
+        a = a + i;
     }
-    speaks() {
-        console.log("hi there" + this.speaks);
-    }
+    return a;
 }
-
-let dog = new Animal("dog", 4, "bhow bhow");
-let cat = new Animal("cat", 4, "meow meow");
-cat.speaks();
