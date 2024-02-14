031bb65bea0e729d0931102743d17b461c4392de
prashant226 <prashantnaraniya2003@gmail.com>
2024-02-14 18:55:29 +0530

    commited

--- WEEK-01/class.js
+++ WEEK-01/class.js
@@ -36,15 +36,33 @@
 // let cat = new Animal("cat", 4, "meow meow");
 // cat.speaks();
 
-const currentData = new Date();
-// console.log(currentData.getDate());
-// console.log(currentData.getYear() + 1900);
-// console.log("time in milliseconds since 1970", currentData.getTime());
+// const currentData = new Date();
+// // console.log(currentData.getDate());
+// // console.log(currentData.getYear() + 1900);
+// // console.log("time in milliseconds since 1970", currentData.getTime());
 
-function calculateSum() {
-    const a = 0;
-    for (let i = 0; i < 10000; i++) {
-        a = a + i;
-    }
-    return a;
+// function calculateSum() {
+//     const a = 0;
+//     for (let i = 0; i < 10000; i++) {
+//         a = a + i;
+//     }
+//     return a;
+// }
+
+function square(n) {
+    return n * n;
 }
+
+function cube(n) {
+    return n * n * n;
+
+}
+
+function sumOfSomething(a, b, fn) {
+    const val1 = fn(a);
+    const val2 = fn(b);
+    return val1 + val2;
+
+}
+
+sumOfSomething(1, 2, cube)
