9f52957c86727c88788c213f10768a2255a3c31c
prashant226 <prashantnaraniya2003@gmail.com>
2024-02-14 19:18:53 +0530

    commited

--- WEEK-01/promises.js
+++ WEEK-01/promises.js
@@ -1,14 +1,31 @@
-function findSum(n) {
-    let ans = 0;
-    for (let i = 0; i < n; i++) {
-        ans += i;
-    }
-    return ans;
+// function findSum(n) {
+//     let ans = 0;
+//     for (let i = 0; i < n; i++) {
+//         ans += i;
+//     }
+//     return ans;
+// }
+
+// function findSumTill100() {
+//     return findSum(100);
+// }
+
+// setTimeout(findSumTill100, 1000)
+// console.log("hello world");
+
+const fs = require('fs');
+
+function kiratReadFile() {
+    return new Promise(function(resolve) {
+        fs.readFile("a.txt", "utf-8", function(err, data) {
+            resolve(data);
+        });
+    })
 }
 
-function findSumTill100() {
-    return findSum(100);
+
+function onDone(data) {
+    console.log(data)
 }
 
-setTimeout(findSumTill100, 1000)
-console.log("hello world");
+kiratReadFile().then(onDone);
