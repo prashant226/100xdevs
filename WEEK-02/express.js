dd850dba60d6625ed3d2ea59acda0763bc50c1c1
prashant226 <prashantnaraniya2003@gmail.com>
2024-02-16 18:48:29 +0530

    commited

---
+++ WEEK-02/express.js
@@ -0,0 +1,13 @@
+// const fs = require('fs');
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+app.get('/', function(req, res) {
+    res.send('hello world');
+})
+
+app.listen(port, function() {
+    console.log(`the app is been running on ${port}`)
+})
