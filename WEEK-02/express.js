f9ef98668180f4920ecbbf3bdd023b3304c53381
prashant226 <prashantnaraniya2003@gmail.com>
2024-02-16 20:03:16 +0530

    commited

--- WEEK-02/express.js
+++ WEEK-02/express.js
@@ -3,18 +3,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/router-handle", function(req, res) {
-    res.send("hello world");
-    res.json({
-        name: "harkirat",
-        age: 21
-    })
-})
+// app.get("/router-handle", function(req, res) {
+//     res.send("hello world");
+//     res.json({
+//         name: "harkirat",
+//         age: 21
+//     })
+// })
 
 
-app.get('/', function(req, res) {
+app.post('/', (req, res) => {
     //db call 1sec
-    res.send('hello world');
+    console.log(req.body)
+    res.send({
+        msg: "2+2=4"
+    });
 })
 
 app.listen(port, function() {
