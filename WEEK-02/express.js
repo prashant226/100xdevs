044cf87ada58ef89db9910ad8e6601809bdb3d4a
prashant226 <prashantnaraniya2003@gmail.com>
2024-02-17 12:23:59 +0530

    commited

--- WEEK-02/express.js
+++ WEEK-02/express.js
@@ -13,14 +13,31 @@ const port = 3000;
 // })
 //middleWare
 // app.use(bodyParser.json());
-app.post('/', (req, res) => {
-    //db call 1sec
-    console.log(req.body);
-    res.send({
-        msg: "2+2=4"
-    });
-})
+// app.post('/', (req, res) => {
+//     //db call 1sec
+//     console.log(req.body);
+//     res.send({
+//         msg: "2+2=4"
+//     });
+// })
 
-app.listen(port, function() {
-    console.log(`the app is been running on ${port}`)
+// app.listen(port, function() {
+//     console.log(`the app is been running on ${port}`)
+// })
+
+function Sum(n) {
+    let ans = 0;
+    for (let i = 0; i <= n; i++) {
+        ans = ans + i;
+    }
+    return ans;
+
+}
+
+
+app.get("/", function(req, res) {
+    const n = req.query.n;
+    const ans = Sum(30);
+    res.send("hi your ans is " + ans);
 })
+app.listen(3000);
---
+++ WEEK-02/express2.js
@@ -0,0 +1,12 @@
+// creating an HTTP server 
+//express
+//node default library
+
+
+// const express = require("express");
+// const app = express();
+
+// app.get("/", function(req, res) {
+//     res.send("hi there");
+// })
+// app.listen(3000);
