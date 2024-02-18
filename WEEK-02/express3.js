a74db824656dbe0fad26cd606b5c1c08d3002202
prashant226 <prashantnaraniya2003@gmail.com>
2024-02-18 13:08:41 +0530

    commited

---
+++ WEEK-02/express3.js
@@ -0,0 +1,50 @@
+// map , filter and arrow
+
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+
+// const sum = (a, b) => {
+//     return a + b;
+
+// }
+// app.get("/", (req, res) => {
+
+// })
+
+// const ans = sum(1, 2);
+// console.log(ans);
+//given an array , give back a array in which every elemnt is mul by 2..
+
+// const input = [1, 2, 3, 4, 5];
+// const newArray = [];
+// for (let i = 0; i < input.length; i++) {
+//     newArray.push(input[i] * 2);
+// }
+
+// console.log(newArray);
+
+
+
+// const ans = input.map(function transform(i) {
+//     return i * 2;
+// });
+// console.log(ans);
+
+
+
+//filtering 
+//what if given a input array give me all even values from it
+const arr = [1, 2, 3, 4];
+
+// function filteringLogic(n) {
+//     if (n % 2 == 0)
+//         return true;
+//     else {
+//         return false;
+//     }
+// }
+// const ans = arr.filter(filteringLogic);
+// console.log(ans);
